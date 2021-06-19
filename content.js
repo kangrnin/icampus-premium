963aca60894399d03e4bd3606500277b3b9a1eca
kangrnin <kangrnin@gmail.com>
2021-06-19 15:51:53 +0000

    Added video speed slider
    - 동영상 속도를 x0.8, x1.0, x1.2, ... 중  선택하던 방식에서 슬라이더 방식으로 업그레이드
    - 0.1 단위로 선택할 수 있음

--- content.js
+++ content.js
@@ -13,6 +13,7 @@ function getWhenExists(selector) {
 }
 
 $(async () => {
+    // intro skip
     const video = await getWhenExists('video.vc-vplay-video1');
     $(video).on('loadedmetadata', async e => { 
         if(e.target.src.includes('/settings/viewer/uniplayer/intro')) {
@@ -20,4 +21,35 @@ $(async () => {
             e.target.currentTime = e.target.duration;
         }
     })
+
+    // speed input slider
+    const speedSetBox = await getWhenExists('div.vc-pctrl-playback-rate-setbox');
+    speedSetBox.innerHTML = '';
+    speedSetBox.style.cssText = `
+        width: 200px;
+        height: 22px;
+        border: 0px;
+        background: transparent;
+        position: absolute;
+        top: 12px;
+        padding-right: 64px;
+    `;
+
+    const speedButton = await getWhenExists('div.vc-pctrl-playback-rate-toggle-btn');
+    const speedInput = document.createElement('input');
+    speedInput.type = 'range';
+    speedInput.value = '1.0';
+    speedInput.min = '0.1';
+    speedInput.max = '2.0';
+    speedInput.step = '0.1';
+    speedButton.onclick = (e) => {
+        speedInput.value = speedButton.innerHTML.slice(2);
+    }
+    speedInput.oninput = (e) => {
+        const speed = e.target.value;
+        speedButton.innerHTML = `x ${speed}`;
+        video.playbackRate = speed;
+    }
+
+    speedSetBox.appendChild(speedInput);
 });
