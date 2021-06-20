ea7f888b92c3ffdd524696f4d0a2320499e07b0e
kangrnin <kangrnin@gmail.com>
2021-06-20 13:39:38 +0000

    Applied ESLint
    ESLint 적용 과정에서 content.js getWhenExists의 비동기 처리 개선

--- background.js
+++ background.js
@@ -1,11 +1,11 @@
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-        chrome.declarativeContent.onPageChanged.addRules([{
-        conditions: [new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: {hostEquals: 'canvas.skku.edu'},
-        })
-        ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-        }]);
-    });
-});
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: { hostEquals: 'canvas.skku.edu' }
+      })
+      ],
+      actions: [new chrome.declarativeContent.ShowPageAction()]
+    }])
+  })
+})
